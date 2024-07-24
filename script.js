48bf48bd2fe16b525db3e81ad9d6b39dd6a2d171
aryan7iitj <106874409+aryan7iitj@users.noreply.github.com>
2024-07-24 09:11:02 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ document.getElementById('predictionForm').addEventListener('submit', async funct
     };
 
     try {
-        const response = await fetch('http://127.0.0.1:8000/predict', {
+        const response = await fetch('https://detailed-tiger-jai-hanumanji-5de75eb4.koyeb.app/predict', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
